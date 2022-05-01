9441771df7b531c45ef14708e92d71446f929e26
GuadaGarcia17 <102746940+GuadaGarcia17@users.noreply.github.com>
2022-05-01 09:44:01 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+//TP1 Guadalupe Sofía García Fariñas K2002
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+    char c = getchar();
+    while(c!=EOF)
+    {
+        if (isupper (c))
+        {
+            c = tolower (c);
+            putchar(c);
+        }
+        else
+        {
+            if (islower (c))
+            {
+                c = toupper (c);
+                putchar(c);
+            }
+            else
+            {
+                if (ispunct (c))
+                putchar(c);
+            }
+        }
+        c = getchar();
+    }
+    return 0;
+}
